908ee1f01b0f4f56bf5be8d488475c1b8e97c557
Andy Mantell <contact@andymantell.com>
2016-01-20 15:15:17 +0000

    Randomizing more of the prototype's data and tweak the behaviour slightly to make it more realistic

--- app/results.js
+++ app/results.js
@@ -1,4 +1,5 @@
 var seedrandom = require('seedrandom');
+var moment = require('moment');
 
 var results = [];
 var numResults = 50;
@@ -11,6 +12,7 @@ var tenures = [
 var firstNames = ['Fred', 'Bob', 'George', 'Julia', 'Sophie', 'David', 'Andy', 'Heather'];
 var lastNames = ['Smith', 'Jones', 'McDonald'];
 
+
 for(var i=1; i<=numResults; i++) {
   var address = [
     i + ' Pretend Street',
@@ -20,14 +22,39 @@ for(var i=1; i<=numResults; i++) {
 
   var rand = seedrandom(i)();
 
-  results.push({
+  // Randomize a date between 1980 and 2015
+  var date = moment.unix(315532800 + (rand * 1104537600));
+
+  var result = {
     'title': address.join(','),
     'address': address,
     'id': i,
     'tenure': tenures[Math.floor(rand * tenures.length)],
-    'title_number': 'DN' + Math.floor(rand * 1000000),
+    'title_number': 'FAKE' + Math.floor(rand * 1000000),
     'owner': firstNames[Math.floor(rand * firstNames.length)] + ' ' + lastNames[Math.floor(rand * lastNames.length)],
-  });
+    'date': date.format('D MMMM YYYY')
+  };
+
+  // Lease term if it's *not* freehold
+  if(result.tenure !== 'freehold') {
+    result.lease_term = 50 + Math.floor(rand * 50);
+  }
+
+  if(rand > 0.3) {
+    result.lender_name = 'Abbey National PLC';
+    result.lender_address = [ 
+      '101 Midsummer Boulevard',
+      'Milton Keynes',
+      'MK9 1AA'
+    ];
+  }
+
+  // If it's newer than 2000, display a price
+  if(date.isAfter('2000-01-01')) {
+    result.price = Math.floor(100 + (rand * 300));
+  }
+
+  results.push(result);
 }
 
 module.exports = results;
